e56b94cda23750479c4e26365a4fb5535c6976ea
andreyscherbin <andrey123scherbin@gmail.com>
2025-06-17 11:45:11 +0300

    add users,sessions queries

--- src/main/java/com/example/java_ifortex_test_task/repository/SessionRepository.java
+++ src/main/java/com/example/java_ifortex_test_task/repository/SessionRepository.java
@@ -1,6 +1,5 @@
 package com.example.java_ifortex_test_task.repository;
 
-import com.example.java_ifortex_test_task.entity.DeviceType;
 import com.example.java_ifortex_test_task.entity.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,9 +8,23 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SessionRepository extends JpaRepository<Session, Long> {
-    @Query(value = "", nativeQuery = true)
-    Session getFirstDesktopSession(DeviceType deviceType);
+    @Query(value = "SELECT s.id," +
+            " s.device_type - 1 AS device_type," +
+            " s.ended_at_utc," +
+            " s.started_at_utc," +
+            " s.user_id" +
+            " FROM sessions AS s" +
+            " WHERE device_type = :deviceTypeCode" +
+            " ORDER BY started_at_utc ASC LIMIT 1", nativeQuery = true)
+    Session getFirstDesktopSession(int deviceTypeCode);
 
-    @Query(value = "", nativeQuery = true)
+    @Query(value = "SELECT s.id," +
+            " s.device_type - 1 AS device_type," +
+            " s.ended_at_utc," +
+            " s.started_at_utc," +
+            " s.user_id" +
+            " FROM sessions s JOIN users u ON s.user_id = u.id " +
+            "WHERE u.deleted = false AND s.ended_at_utc < :endDate " +
+            "ORDER BY s.started_at_utc DESC", nativeQuery = true)
     List<Session> getSessionsFromActiveUsersEndedBefore2025(LocalDateTime endDate);
 }
--- src/main/java/com/example/java_ifortex_test_task/repository/UserRepository.java
+++ src/main/java/com/example/java_ifortex_test_task/repository/UserRepository.java
@@ -1,16 +1,23 @@
 package com.example.java_ifortex_test_task.repository;
 
-import com.example.java_ifortex_test_task.entity.DeviceType;
 import com.example.java_ifortex_test_task.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query(value = "", nativeQuery = true)
-    List<User> getUsersWithAtLeastOneMobileSession(DeviceType deviceType);
+    @Query(value = "SELECT DISTINCT u.* " +
+            "FROM users u " +
+            "JOIN sessions s ON u.id = s.user_id " +
+            "WHERE s.device_type = :deviceTypeCode", nativeQuery = true)
+    List<User> getUsersWithAtLeastOneMobileSession(int deviceTypeCode);
 
-    @Query(value = "", nativeQuery = true)
+    @Query(value = "SELECT u.*, " +
+            "COUNT(s.id) AS session_count " +
+            "FROM users u " +
+            "LEFT JOIN sessions s ON u.id = s.user_id " +
+            "GROUP BY u.id " +
+            "ORDER BY session_count DESC " +
+            "LIMIT 1", nativeQuery = true)
     User getUserWithMostSessions();
 }
